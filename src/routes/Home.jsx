977e9836a0899ba896ed61e86218e9dc6e5b1fb6
dErnandoV7 <d.ernandov7@gmail.com>
2024-03-23 08:36:02 -0300

    exibindo posts na home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
       );
 
       const data = response.data;
-      
+      setPosts(data);
     } catch (error) {
       console.log(`Houve um erro: ${error}`);
     }
@@ -26,7 +26,24 @@ const Home = () => {
     getPosts();
   }, []);
 
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Últimos posts</h1>
+      {!posts.length ? (
+        <p>Carregando..</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Home;
