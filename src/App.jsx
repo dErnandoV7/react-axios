3fa6f07e5597bebb97b9af1a6b5bb64f7bef2772
dErnandoV7 <d.ernandov7@gmail.com>
2024-03-23 08:10:39 -0300

    criando barra de navegacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Outlet, NavLink } from "react-router-dom";
 
+import NavBar from "./components/NavBar";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <div className="container">
         <Outlet />
       </div>
